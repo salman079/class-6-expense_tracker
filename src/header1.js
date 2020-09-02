3c31a6506356c0693ed812bb72018b554bca49ce
salmansiddiqui <salman079@gmail.com>
2020-09-02 21:06:02 +0300

    sep 2

---
+++ src/header1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header1(){
+    return (
+        <div>
+            <h3 className='center'>
+                Expense Tracker by Salman Siddiqui
+            </h3>
+        </div>
+    )
+}
+
+export default Header1;
